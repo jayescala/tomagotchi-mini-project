f32acecb96ab6cc77a4cb2bd919c1e4311875b35
Jay Escala <Jonathan.J.Escala@gmail.com>
2018-06-28 14:31:54 -0500

    Updated user interface, added gaming feature.

--- js/app.js
+++ js/app.js
@@ -1,311 +1,398 @@
-console.log("The javascript is loaded.");
+console.log("Hello World!");
 
 // tomagotchi-mini-project
 
-let gameRunning = false;
-let lights = true;
+// Controls
+let petFaint = false;
+let userChoice = 0;
+let petChoice = 0;
+let foodThrown = 0;
+let lightsOn = true;
+let gamesPlayed = 0;
 
-
-// Tomagotchi Class
+// Classes
 class Pet {
 	constructor(name){
 		this.name = name;
 		this.age = 0;
-		this.hunger = 10;
-		this.energy = 10;
-		this.happiness = 10;
-	}
-	cry(){
-		console.log("**krreee** **krreee***");
+		this.hunger = 100;
+		this.energy = 100;
+		this.happiness = 100;
 	}
 }
 
-const createPet = () => {
-	$(".create").hide();
-	$(".sprite").remove();
-	let name = prompt("What would you like to name your Tomagotchi?", "Pikachu");
-	const pet = new Pet(name);
-	addPetSprite(pet);
-	createButtons(pet);
-	createStatus(pet);
+// Functions
+const renderStartPage = () => {
+	$(".side-displays").addClass("transparent");
+	$(".windows").hide();
+	$("#pet-sprite").hide();
+	$("#pet-food").hide();
+	startScreenAnimation();
+	$("#pet-sprite").addClass("pikachu-animation-1");
+	$("#grass-sprite").addClass("grass-animation-1");
+	$("#water-sprite").addClass("water-animation-1");
+	$("#fire-sprite").addClass("fire-animation-1");
+	startPetAnimations();
+	$("#start-button").on("click", renderPetNamePage)
+}
+
+const startScreenAnimation = () => {
+	const startScreenAnimation = setInterval(function(){
+		$("#start-screen").fadeToggle();
+	}, 750);
+}
+
+const renderPetNamePage = () => {
+	$("#start-screen").remove();
+	$("#display").addClass("home");
+	$("#notification-display").removeClass("transparent");
+	$("#pet-name-notification-window").show();
+	$("#start-button").off("click");
+}
+
+const createPet = (pet) => {
+	calculateStatus(pet);
+	$("#pet-sprite").fadeIn();
+}
+
+const calculateStatus = (pet) => {
+	$("#menu-display").addClass("transparent");
 	calculateAge(pet);
 	calculateHunger(pet);
 	calculateEnergy(pet);
 	calculateRest(pet);
 	calculateHappiness(pet);
-}
-
-const addPetSprite = (pet) => {
-	$petSprite = $("<div/>").addClass("sprite animation1");
-	$(".home").append($petSprite);
-	$(".sprite").css("margin-top", 0);
-	$(".sprite").css("margin-left", 0);
-	startPetAnimation(pet);
-}
-
-const startPetAnimation = (pet) => {
-	let marginTop = 0;
-	let marginLeft = 0;
-	const petAnimation = 
-		setInterval(function(){
-			if(pet.hunger <= 0 || pet.energy <= 0 || pet.happiness <= 0){
-					clearInterval(petAnimation);
-					$(".sprite").css("opacity", 0.5);
-					$(".status").hide();
-					$(".interactions").hide();
-					$(".end-game").hide();
-					$(".instructions").hide();
-					$(".game-module").hide();
-					$petDeath = $("<h5/>").addClass("pet-death").text("Your pet has fainted.");
-					$("#module").append($petDeath);
-					$tryAgain = $("<button/>").addClass("try-again").text("Try Again");
-					$("#module").append($tryAgain);
-					$(".try-again").on("click", (event) => {
-						location.reload(event);
-					})
-			} else {
-				let randomNumTop = Math.floor((Math.random() * 21)-10);
-				let randomNumLeft = Math.floor((Math.random() * 21)-10);
-				
-				marginTop += randomNumTop;
-				marginTop = Math.max(-22, marginTop);
-				marginTop = Math.min(66, marginTop);
-	
-					marginLeft += randomNumLeft;
-					marginLeft = Math.max(-66, marginLeft);
-					marginLeft = Math.min(66, marginLeft);
-	
-	
-					$(".sprite").css("margin-top", (marginTop) + "%");
-					$(".sprite").css("margin-left", (marginLeft) + "%");
-					$(".sprite").toggleClass("animation2");
-				}
-		}, 500);
-}
-
-const createButtons = (pet) => {
-	$petInteractions = $(".interactions");
-	$petInteractions.append($("<button/>").addClass("feed").text("Feed me!"));
-	$(".feed").on("click", () => {
-		if(pet.hunger < 10){
-			pet.hunger++;
-			$(".hungerbar").css("width", (pet.hunger * 10) + "px");
-		}
-	});
-
-	$petInteractions.append($("<button/>").addClass("lights").text("Turn off lights"));
-	$(".lights").on("click", turnOffLights);
-
-	const $gameSelections = $("<div/>").addClass("game-selections");
-	$gameSelections.css("display","flex");
-	$gameSelections.css("flex-direction","row");
-	const $gameModule = $("<div/>").addClass("game-module");
-	$module = $("#module");
-	$petInteractions.append($("<button/>").addClass("play").text("Play with me!"));
-	$(".play").on("click", playGame);
-	$gameInteractions = $("<div/>").addClass("game-interactions");
-	$("#module").append($gameInteractions);
-	$gameInteractions.append($("<button/>").addClass("game-buttons end-game").text("End Game").hide());
-	$(".end-game").on("click", endGame);
-	$gameInteractions.append($("<button/>").addClass("game-buttons instructions").text("Show Instructions").hide());
-	$(".instructions").on("click", displayInstructions);
-	$module.append($gameModule);
-	$gameModule.hide();
-	$gameModule.append($("<h5/>").text("Choose one:"));
-	$gameModule.append($gameSelections);
-	$gameSelections.append($("<div/>").addClass("selection grass1"));
-	$gameSelections.append($("<div/>").addClass("selection water1"));
-	$gameSelections.append($("<div/>").addClass("selection fire1"));
+	calculateFaint(pet);
 }
 
 const calculateAge = (pet) => {
-	const ageMeter =
-		setInterval(function(){
-			pet.age++;
-			if(pet.age === 1){
-				$(".age-stat").text(pet.age + " minute");
-			} else {
-				$(".age-stat").text(pet.age + " minutes")
-			}
-			
-		}, 60000);
+	$("#age-value").text(pet.age + " minutes");
+	const ageMeter = setInterval(function(){
+		pet.age++;
+		if(pet.age === 1){
+			$("#age-value").text(pet.age + " minute");
+		} else {
+			$("#age-value").text(pet.age + " minutes");
+		}
+		if (petFaint === true) {
+			clearInterval(ageMeter);
+		}
+	}, 60000);
 }
 
 const calculateHunger = (pet) => {
-	const hungerMeter =
-		setInterval(function(){
-			if(pet.hunger > 0){
-				pet.hunger--;
-				$(".hungerbar").css("width", (pet.hunger * 10) + "px");
-			}
-		}, 10000);
+	const hungerMeter = setInterval(function(){
+		if((pet.hunger < 100) && (lightsOn === true) && (foodThrown > 0)){
+			pet.hunger += 10;
+			pet.hunger = Math.min(100, pet.hunger);
+			$("#hunger-statbar").css("width", (pet.hunger + "%"));
+			foodThrown--;
+		}
+		if(pet.hunger > 0){
+			pet.hunger--;
+			$("#hunger-statbar").css("width", (pet.hunger + "%"));
+		} else if(petFaint === true) {
+			clearInterval(hungerMeter);
+		}
+	}, 1000);
 }
 
 const calculateHappiness = (pet) => {
-	const happinessMeter =
-		setInterval(function(){
-			if(pet.happiness > 0){
-				pet.happiness--;
-				$(".happinessbar").css("width", (pet.happiness * 10) + "px");;
-			}
-		}, 30000);
-}
-
-const playGame = () => {
-	$(".interactions").hide();
-	$(".status").hide();
-	$(".end-game").show();
-	$(".instructions").show();
-	$(".game-module").show();
-	gameRunning = true;
-	startSelectionAnimations();
-}
-
-const endGame = () => {
-	$(".interactions").show();
-	$(".status").show();
-	$(".end-game").hide();
-	$(".instructions").hide();
-	$(".game-module").hide();
-	clearInstructions();
-	gameRunning = false;
-}
-
-const displayInstructions = () => {
-	$(".case").append($("<ul/>").addClass("written-instructions"));
-	$(".written-instructions").text("Instructions: Select one of 3 choices");
-	$(".written-instructions").append($("<li/>").addClass("rule").text("Grass beats Water"));
-	$(".written-instructions").append($("<li/>").addClass("rule").text("Water beats Fire"));
-	$(".written-instructions").append($("<li/>").addClass("rule").text("Fire beats Grass"));
-	$(".instructions").text("Hide Instructions");
-	$(".instructions").off("click").on("click", clearInstructions);
-}
-
-const clearInstructions = () => {
-	$(".written-instructions").remove();
-	$(".instructions").text("Show Instructions");
-	$(".instructions").off("click").on("click", displayInstructions);
-}
-
-const startSelectionAnimations = () => {
-	startGrassAnimation();
-	startWaterAnimation();
-	startFireAnimation();
-}
-
-const startGrassAnimation = () => {
-	const petAnimation = 
-		setInterval(function(){
-			if(gameRunning === true){
-				$(".grass1").toggleClass("grass2");
-			} else {
-				clearInterval(petAnimation);
-			}
-		}, 250);
-}
-
-const startWaterAnimation = () => {
-	const petAnimation = 
-		setInterval(function(){
-			if(gameRunning === true){
-				$(".water1").toggleClass("water2");
-			} else {
-				clearInterval(petAnimation);
-			}
-		}, 250);
-}
-
-const startFireAnimation = () => {
-	const petAnimation = 
-		setInterval(function(){
-			if(gameRunning === true){
-				$(".fire1").toggleClass("fire2");
-			} else {
-				clearInterval(petAnimation);
-			}
-		}, 250);
+	const happinessMeter = setInterval(function(){
+		if((pet.happiness < 100) && (gamesPlayed > 0)){
+			pet.happiness += 15;
+			pet.happiness = Math.min(100, pet.happiness);
+			$("#happiness-statbar").css("width", (pet.happiness + "%"));
+			gamesPlayed--;
+		}
+		if(pet.happiness > 0){
+			pet.happiness--;
+			$("#happiness-statbar").css("width", (pet.happiness + "%"));
+		}
+		if(petFaint === true) {
+			clearInterval(happinessMeter);
+		}
+	}, 2000);
 }
 
 const calculateEnergy = (pet) => {
-	const energyMeter =
-		setInterval(function(){
-			if((pet.energy > 0) && (lights === true)){
+	const energyMeter = setInterval(function(){
+		if((pet.energy > 0) && (lightsOn === true)){
 			pet.energy--;
-			}
-			$(".energybar").css("width", (pet.energy * 10) + "px");
-		}, 20000);
+			$("#energy-statbar").css("width", (pet.energy + "%"));
+		}
+		if(petFaint === true){
+			clearInterval(energyMeter);
+		}
+	}, 3000);
 }
 
 const calculateRest = (pet) => {
-	const energyMeter =
-		setInterval(function(){
-			if((pet.energy < 10) && (lights === false)){
+	const energyMeter = setInterval(function(){
+			if((pet.energy < 100) && (lightsOn === false)){
 			pet.energy++;
 			}
-			$(".energybar").css("width", (pet.energy * 10) + "px");
-		}, 2000);
+			if(petFaint === true) {
+				clearInterval(energyMeter);
+			}
+			$("#energy-statbar").css("width", (pet.energy + "%"));
+		}, 200);
+}
+
+const calculateFaint = (pet) => {
+	const faintCheck = setInterval(function(){
+		if((pet.hunger <= 0) || pet.energy <= 0 || pet.happiness <= 0){
+			petFaint = true;
+			$("#pet-sprite").addClass("fade");
+			$("#display").addClass("dark-background");
+			$("#pet-food").hide();
+			$("#menu-display").addClass("transparent");
+			$(".windows").hide();
+			$("#faint-window").show();
+			$("#notification-display").removeClass("transparent");
+			$("#try-again-button").on("click", (event) => {
+				location.reload(event);
+			});
+		}
+	}, 1000);
+}
+
+
+const startPetAnimations = () => {
+	let marginTop = 0;
+	let marginRight = 0;
+
+	const petAnimation = setInterval(function(){
+		if(lightsOn === false || petFaint === true){
+			clearInterval(petAnimation);
+			$("#pet-sprite").removeClass("pikachu-animation-2");
+		} else {
+			let randomNumTop = (Math.floor(Math.random() * 22))-10;
+			let randomNumRight = (Math.floor(Math.random() * 21))-10;
+
+			marginTop += randomNumTop;
+			marginTop = Math.max(-60, marginTop);
+			marginTop = Math.min(125, marginTop);
+
+			marginRight += randomNumRight;
+			marginRight = Math.max(-125, marginRight);
+			marginRight = Math.min(125, marginRight);
+
+			$("#pet-sprite").css("margin-top", (marginTop) + "px");
+			$("#pet-sprite").css("margin-right", (marginRight) + "px");
+			$("#pet-sprite").toggleClass("pikachu-animation-2");
+			$("#grass-sprite").toggleClass("grass-animation-2");
+			$("#water-sprite").toggleClass("water-animation-2");
+			$("#fire-sprite").toggleClass("fire-animation-2");
+
+		}
+	}, 500);
+}
+
+const showFood = () => {
+	$("#pet-food").show();
+	$("#a-button").on("click", throwFood);
+	$("#down-button").on("click", hideFood);
+}
+
+const hideFood = () => {
+	$("#pet-food").hide();
+	$("#a-button").off("click");
+	$("#up-button").off("click").on("click", showFood);
+	$("#down-button").off("click");
+}
+
+const throwFood = () => {
+	$("#pet-food").slideUp(500);
+	$("#a-button").off("click");
+	$("#up-button").off("click").on("click", showFood);
+	$("#down-button").off("click");
+	foodThrown++;
+
+}
+
+const hideMenuDisplay = () => {
+	$("#menu-display").addClass("transparent");
+	$("#select-button").off("click").on("click", showMenuDisplay);
+}
+
+const showMenuDisplay = () => {
+	$("#menu-display").removeClass("transparent");
+	$("#select-button").off("click").on("click", hideMenuDisplay);
 }
 
 const turnOffLights = () => {
-	$(".home").css("background", "linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.8)), url(https://orig00.deviantart.net/a88f/f/2014/325/6/6/or_as_battle_background_1b_by_phoenixoflight92-d874gjl.png) no-repeat");
-	$(".home").css("background-size", "cover");
-	$(".sprite").css("opacity", 0.7);
-	$(".lights").text("Turn on lights");
-	$(".feed").hide();
-	$(".play").hide();
-	$(".lights").off("click").on("click", turnOnLights);
-	lights = false;
+	$("#display").addClass("dark-background");
+	$("#pet-sprite").addClass("fade");
+	lightsOn = false;
+	$("#a-button").off("click");
+	$("#up-button").off("click");
+	$("#right-button").off("click");
+	$("#pet-food").hide();
+	$("#pet-food").addClass("transparent");
+	$("#start-button").off("click").on("click", turnOnLights);
 }
 
 const turnOnLights = () => {
-	$(".home").css("background", "");
-	$(".sprite").css("opacity", 1);
-	$(".lights").text("Turn off lights");
-	$(".feed").show();
-	$(".play").show();
-	$(".lights").off("click").on("click", turnOffLights);
-	lights = true;
+	$("#display").removeClass("dark-background");
+	$("#pet-sprite").removeClass("fade");
+	lightsOn = true;
+	$("#a-button").off("click");
+	$("#up-button").on("click", showFood);
+	$("#right-button").on("click", startGame);
+	$("#pet-food").hide();
+	$("#pet-food").removeClass("transparent");
+	startPetAnimations();
+	$("#start-button").off("click").on("click", turnOffLights);
 }
 
-const createStatus = (pet) => {
-	$status = $(".status");
-	$status.css("background-color", "#f2f3f5");
-	$status.css("border", "2px solid #bebbbb");
-	$labelColumn = $("<div/>").addClass("label-column");
-	$status.append($labelColumn);
-	$labelColumn.append($("<div/>").addClass("name").text("Name: "));
-	$labelColumn.append($("<div/>").addClass("age").text("Age: "));
-	$labelColumn.append($("<div/>").addClass("hunger").text("Hunger: "));
-	$labelColumn.append($("<div/>").addClass("energy").text("Energy: "));
-	$labelColumn.append($("<div/>").addClass("happiness").text("Happiness: "));
+const startGame = () => {
+	$("#notification-display").removeClass("transparent");
+	$("#game-window").show();
+	$("#right-button").off("click");
+	$("#left-button").on("click", exitGame);
+	$("#start-button").off("click");
 
-	$statColumn = $("<div/>").addClass("stat-column");
-	$status.append($statColumn);
-	$statColumn.append($("<div/>").addClass("name-stat").text(pet.name));
-	$statColumn.append($("<div/>").addClass("age-stat").text(pet.age + " minutes"));
-	$statColumn.append($("<div/>").addClass("hunger-stat").text(""));
-	createHungerBar(pet);
-	$statColumn.append($("<div/>").addClass("energy-stat").text(""));
-	createEnergyBar(pet);
-	$statColumn.append($("<div/>").addClass("happiness-stat").text(""));
-	createHappinessBar(pet);
 }
 
-const createHungerBar = (pet) => {
-	$hungerBar = $("<div/>").addClass("hungerbar");
-	$(".hunger-stat").append($hungerBar);
+const exitGame = () => {
+	$("#notification-display").addClass("transparent");
+	$("#game-window").hide();
+	$("#game-results-window").hide();
+	$("#left-button").off("click");
+	$("#right-button").on("click", startGame);
+	$("#start-button").on("click", turnOffLights);
 }
 
-const createEnergyBar = (pet) => {
-	$energyBar = $("<div/>").addClass("energybar");
-	$(".energy-stat").append($energyBar);
+
+const userChooseGrass = () => {
+	$("#user-choice-sprite").addClass("grass-animation-1");
+	$("#user-choice-label").text("Grass");
+	return 1;
 }
 
-const createHappinessBar = (pet) => {
-	$happinessBar = $("<div/>").addClass("happinessbar");
-	$(".happiness-stat").append($happinessBar);
+const userChooseWater = () => {
+	$("#user-choice-sprite").addClass("water-animation-1");
+	$("#user-choice-label").text("Water");
+	return 2;
 }
 
-$(".create").on("click", createPet);
+const userChooseFire = () => {
+	$("#user-choice-sprite").addClass("fire-animation-1");
+	$("#user-choice-label").text("Fire");
+	return 3;
+}
 
-// Game Controls
+const petChooseGrass = () => {
+	$("#pet-choice-sprite").addClass("grass-animation-1");
+	$("#pet-choice-label").text("Grass");
+	return 1;
+}
+
+const petChooseWater = () => {
+	$("#pet-choice-sprite").addClass("water-animation-1");
+	$("#pet-choice-label").text("Water");
+	return 2;
+}
+
+const petChooseFire = () => {
+	$("#pet-choice-sprite").addClass("fire-animation-1");
+	$("#pet-choice-label").text("Fire");
+	return 3;
+}
+
+const generatePetChoice = () => {
+	const randomNum = Math.floor((Math.random() * 3)+1);
+	if(randomNum === 1){
+		petChoice = petChooseGrass();
+	} else if(randomNum === 2){
+		petChoice = petChooseWater();
+	} else if(randomNum === 3){
+		petChoice = petChooseFire();
+	}
+}
+
+const calculateWinner = () => {
+	if(userChoice === petChoice){
+		$("#results").text("It was a draw!");
+	} else if((userChoice === 1 && petChoice === 2) || (userChoice === 2 && petChoice === 3) || (userChoice === 3 && petChoice === 1)){
+		$("#results").text("You won!");
+	} else {
+		$("#results").text("You lost!");
+	}
+}
+
+const showResultsPage = () => {
+	$("#game-window").hide();
+	$("#game-results-window").show();
+	if(userChoice === 1){
+		userChoice = userChooseGrass();
+	} else if(userChoice === 2){
+		userChoice = userChooseWater();
+	} else if(userChoice === 3){
+		userChoice = userChooseFire();
+	}
+	gamesPlayed++;
+	generatePetChoice();
+	calculateWinner();
+}
+
+const removeSpriteClasses = () => {
+	$("#user-choice-sprite").removeClass("grass-animation-1");
+	$("#user-choice-sprite").removeClass("water-animation-1");
+	$("#user-choice-sprite").removeClass("fire-animation-1");
+
+	$("#pet-choice-sprite").removeClass("grass-animation-1");
+	$("#pet-choice-sprite").removeClass("water-animation-1");
+	$("#pet-choice-sprite").removeClass("fire-animation-1");
+}
+
+const setHomeButtons = () => {
+	$("#select-button").on("click", hideMenuDisplay);
+	$("#start-button").on("click", turnOffLights);
+	$("#right-button").on("click", startGame);
+	$("#up-button").on("click", showFood);
+
+}
+// Buttons
+
+$("#pet-name-submit").on("click", (event) => {
+	event.preventDefault();
+
+	const petName = $("#pet-name-input").val();
+	const pet = new Pet(petName);
+	createPet(pet);
+	$("#name-value").text(petName);
+	$("#notification-display").addClass("transparent");
+	$("#pet-name-notification-window").hide();
+	$("#menu-display").removeClass("transparent");
+	$("#status-window").show();
+	setHomeButtons();
+});
+
+$("#grass-sprite").on("click", (event) => {
+	userChoice = 1;
+	showResultsPage();
+});
+
+$("#water-sprite").on("click", (event) => {
+	userChoice = 2;
+	showResultsPage();
+});
+
+$("#fire-sprite").on("click", (event) => {
+	userChoice = 3;
+	showResultsPage();
+});
+
+$("#results-button").on("click", (event) => {
+	userChoice = 0;
+	petChoice = 0;
+	$("#game-results-window").hide();
+	removeSpriteClasses();
+	exitGame();
+	startGame();
+});
+
+// Application Initializer
+$(document).ready(renderStartPage);
